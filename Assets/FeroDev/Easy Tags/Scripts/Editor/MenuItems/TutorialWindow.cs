6214194bf35db775d79bbe4a816648d1495baf8a
FeroDev <ferodev@proton.me>
2026-09-18 23:35:10 +0300

    change online documentation link

--- Assets/FeroDev/Easy Tags/Scripts/Editor/MenuItems/TutorialWindow.cs
+++ Assets/FeroDev/Easy Tags/Scripts/Editor/MenuItems/TutorialWindow.cs
@@ -25,7 +25,7 @@ namespace EasyTags
         // Paths to some sources
         private const string ImagesFolder = "Assets/FeroDev/Easy Tags/Icons/Images";
         private const string PDFDocumentationPath = "Assets/FeroDev/Easy Tags/Documentation/Documentation.pdf";
-        private const string OnlineDocumentationLink = "https://ferodev0.github.io/EasyTagsDocumentation/";
+        private const string OnlineDocumentationLink = "https://ferodev0.github.io/easy-tags-documentation/";
 
         private static readonly Color AccentColor = Color.cyan; // matches TagManagerWindow's cyan header/tag accent
 
